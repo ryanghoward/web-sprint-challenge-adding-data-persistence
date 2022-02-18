8a4c37c0f5416044efd923fc6352f1d07a9d07c6
Ryan Howard <ryanghoward@protonmail.com>
2022-02-18 11:20:30 -0800

    Models and Routers built

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,23 @@
 // build your `/api/resources` router here
 const express = require("express");
 const router = express.Router();
+const Resources = require("./model");
+
+router.get("/", (req, res, next) => {
+  Resources.getAll()
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newResource = await Resources.create(req.body);
+    res.status(201).json(newResource);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
